8befe5f593cf721d6ce74ddfd62bc3c6c04c409f
QuinnPooch <pucciquinn@gmail.com>
2022-02-13 13:50:46 -0700

    completed tag routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -6,6 +6,14 @@ const { Category, Product } = require('../../models');
 router.get('/', (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  Category.findAll({
+    include: [Product]
+  })
+  .then(categoryData => {
+    res.json(categoryData)
+  }).catch(err => {
+    res.status(500).json(err)
+  })
 });
 
 router.get('/:id', (req, res) => {
